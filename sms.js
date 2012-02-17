555fafd217b7a7e640bf5f6d9cf5b3cd62a2d5e0
Chris Newton <chrisnewtn@gmail.com>
2012-02-17 15:58:48 +0000

    updated sms skeleton

--- sms.js
+++ sms.js
@@ -1,5 +1,12 @@
-var methods = ['who'];
-
-switch(currentCall.initialText) {
-	
+if (methods[currentCall.initialText] != undefined) {
+	say(methods[currentCall.initialText]());
+} else {
+	say("Not a valid method call.");
 }
+
+
+var methods = {
+	who: function() {
+		say('Valid method call.');
+	}
+};
