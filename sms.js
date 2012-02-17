ad0c7b668c0fde96de44bed0104f6f41221c5dba
Chris Newton <chrisnewtn@gmail.com>
2012-02-17 16:03:40 +0000

    updated

--- sms.js
+++ sms.js
@@ -1,12 +1,11 @@
-if (methods[currentCall.initialText] != undefined) {
-	say(methods[currentCall.initialText]());
-} else {
-	say("Not a valid method call.");
-}
-
-
 var methods = {
 	who: function() {
 		say('Valid method call.');
 	}
 };
+
+if (methods[currentCall.initialText] != undefined) {
+	say(methods[currentCall.initialText]());
+} else {
+	say("Not a valid method call.");
+}
