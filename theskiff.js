bd0c5058641011221040447b2f52383ac0c32822
Aaron Acerboni <a.acerboni@gmail.com>
2012-02-17 14:39:50 +0000

    javascript port

---
+++ theskiff.js
@@ -0,0 +1,68 @@
+var selection = menu(
+                    "Hi, I'm the Skiff telephone robot, how can I help you?",
+                    [
+                        (1, meeting, "for meeting room bookings"),
+                        (2, rickroll, "if this is a marketing call"),
+                        (3, joining, "if you are interested in becoming a member of the skiff"),
+                        (4, skiff, "to speak to one of our members working at the Skiff now"),
+                        (5, buildbrighton, "if you wish to speak to someone at the Build Brighton hacker space"),
+                        (6, other, "for all other enquiries"),
+                        (9, urgent, "if you have an urgent problem")
+                    ]
+                );
+
+answer();
+wait(500);
+
+function menu(greeting, options){
+
+}
+
+function meeting(){
+    say("The easiest way to check meeting room availability is online at the skiff dot org slash bookings. Alternatively, please hold the line to be transfered to Wired Sussex ");
+    wait(2000);
+    say("Please wait while we transfer your call to wired sussex.");
+    transfer(["+441273692888"]); // wired sussex landline
+}
+
+function skiff(){
+    say("Please wait while we transfer your call to the skiff landline.");
+    transfer(["+441273697366"]); // Skiff landline
+}
+
+function other(){
+    say("The best way to reach the skiff is by email on bookings at the skiff dot org. Alternatively, please hold the line to be transferred to our parent organisation, Wired Sussex ");
+    wait(2000);
+    say("Please wait while we transfer your call to wired sussex.");
+    transfer(["+441273692888"]); // wired sussex landline
+}
+
+function buildbrighton(){
+    say("Please wait while we transfer your call to the Build Brighton hacker space.");
+    transfer(["+441273358263"]); // build brighton
+}
+
+function joining(){
+    say("def interested in membership of the skiff please do come and visit and do a free trial day here. We are open between 10 A M and 6 P M on week days and we are at 6 Gloucester Road in brighton");
+    wait(500);
+    say("Make sure you get on the waiting list too at the skiff dot org slash pricing");
+    wait(500);
+    say("You can also email the skiff on bookings at the skiff dot org");
+    wait(500);
+    say("We look forwards to meeting you");
+}
+
+function rickroll(){
+    say("Please wait while we redirect you to mister Ast lee in the purchasing deparment");
+    wait(500);
+    while(true){
+        say("http://hosting.tropo.com/75249/www/audio/rickroll.mp3");
+        say("Hello, this is My Astley speaking, I just wanted to say" );
+    }
+}
+
+function urgent(){
+    say("<speak>The emergency contact for the Skiff is currently <prosody rate='-90%'>Jonathan Markwell</prosody>. His mobile number is <prosody rate='-50%'>oh double seven double six, 021485</prosody>. That's <prosody rate='-50%'>07766021485</prosody>.</speak>");
+    // #transfer(["+447766021485"]), {"callerID": "666"});
+    // #transfer(["+447951261227"], {"callerID": "666", "network": "SMS"});
+}
