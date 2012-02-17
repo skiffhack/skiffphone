72e25d97d9b2de0abff7785f96d261cb0ca2c7e6
Chris Newton <chrisnewtn@gmail.com>
2012-02-17 16:31:43 +0000

    work dammit

--- sms.js
+++ sms.js
@@ -1,8 +1,14 @@
-if (currentCall.initialText == 'hello') {
+var result = ask("What's your favorite color?  Choose from red, blue or green.", {
+   choices:"red, blue, green"
+});
+say("You said " + result.value);
+log("They said " + result.value);
+
+/*if (currentCall.initialText == 'hello') {
 	say("Hell yourself.")
 } else {
 	say("I don't understand.");
-}
+}*/
 
 /*var methods = {
 	who: function() {
